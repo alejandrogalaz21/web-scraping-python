cdbd077e1975d9e7abd1859ac39c85811653e639
alejandro <alejandrogalaz21@gmail.com>
2021-07-28 16:45:40 -0500

    print pagination result

--- index.py
+++ index.py
@@ -1,14 +1,18 @@
 import requests
 
-r = requests.get('https://quotes.toscrape.com/')
 
-html = r.text
 
-with open('quotes.txt', 'w') as f:
-  for line in html.split('\n'):
-    if '<span class="text" itemprop="text">' in line:
-      line = line.replace('<span class="text" itemprop="text">“', '').replace('”</span>', '')
-      line = line.strip()
-      print(line)
-      f.write(line)
-      f.write('\n')
+for page in range(1,11):
+  url = 'https://quotes.toscrape.com/{page}'
+  r = requests.get('https://quotes.toscrape.com/')
+  html = r.text
+  with open('quotes.txt', 'a', encoding='utf-8') as f:
+    for line in html.split('\n'):
+      if '<span class="text" itemprop="text">' in line:
+        line = line.replace('<span class="text" itemprop="text">“', '').replace('”</span>', '')
+        line = line.strip()
+        print(line)
+        print('\n')
+        f.write(line)
+        f.write('\n')
+        f.write('\n')
